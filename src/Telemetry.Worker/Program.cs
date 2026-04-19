c0e1f5ab83b955869c2bd106dcc93949273f2b6b
DawidMilerski <dawidmilerski@gmail.com>
2026-04-19 12:17:02 +0200

    del worker

--- src/Telemetry.Worker/Program.cs
+++
@@ -1,9 +0,0 @@
-using Telemetry.Worker.Services;
-
-var builder = Host.CreateApplicationBuilder(args);
-
-builder.Services.AddSingleton<InfluxWriter>();
-builder.Services.AddHostedService<RabbitMqConsumerService>();
-
-var host = builder.Build();
-host.Run();
